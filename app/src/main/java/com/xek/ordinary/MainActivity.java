7e1276433da98c830a421f9555f604fa9ac00445
unknown <>
2016-02-04 11:03:37 +0800

    build project

---
+++ app/src/main/java/com/xek/ordinary/MainActivity.java
@@ -0,0 +1,16 @@
+package com.xek.ordinary;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView();
+
+    }
+
+}
